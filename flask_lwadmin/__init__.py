01dc460f9daff83dafd3b85e1ca68ef39709eb21
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-08-29 13:45:31 +0200

    navbar class

--- flask_lwadmin/__init__.py
+++ flask_lwadmin/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding=utf8
 
-from flask import Blueprint, current_app, url_for
+from flask import Blueprint
 
 
 class LwAdmin(object):
+
     def __init__(self, app=None):
         if app is not None:
             self.init_app(app)
@@ -19,10 +20,50 @@ class LwAdmin(object):
 
         app.register_blueprint(blueprint)
 
-        menu = Menu()
 
-class Menu:
-    _menu = []
+class Navbar:
+    NO_URL = 0
+    URL_INTERNAL = 1
+    URL_EXTERNAL = 2
+    DIVIDER = 3
 
-    def add_item(self, name, url, type=None, view=None, parent=None):
-        self._menu.append({'name': name, 'url': url, 'view': view})
+    _navbar = {'brand': 'Project Name', 'username': None, 'items': [], 'profile_items': []}
+    _items = {}
+
+    def set_brand(self, brand):
+        self._navbar['brand'] = brand
+
+    def set_username(self, username):
+        self._navbar['username'] = username
+
+    def add_item(self, key, label, url=None, type=None, controller=None, disabled=False):
+        item = self.__create_base_item(label, url, type, controller, disabled)
+        item['menus'] = []
+        self._items[key] = item
+        self._navbar['items'].append(key)
+
+    def add_sub_item(self, parent_key, key, label, url=None, type=None, controller=None, disabled=False):
+        item = self.__create_base_item(label, url, type, controller, disabled)
+        self._items[key] = item
+        self._items[parent_key]['menus'].append(key)
+
+    def add_profile_item(self, key, label, url=None, type=None, controller=None, disabled=False):
+        item = self.__create_base_item(label, url, type, controller, disabled)
+        self._items[key] = item
+        self._navbar['profile_items'].append(key)
+
+    def get_item(self, key):
+        return self._items[key]
+
+    def get_navbar(self):
+        return self._navbar
+
+    def __create_base_item(self, label, url=None, type=None, controller=None, disabled=False):
+        if type is None:
+            type = self.NO_URL
+
+        item = {'label': label, 'type': type, 'url': url, 'disabled': disabled}
+        if controller is not None:
+            item['controller'] = controller
+
+        return item
