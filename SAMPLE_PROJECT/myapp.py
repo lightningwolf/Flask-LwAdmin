d27cddd8b8bb1df4cb9ac0092c009213f06acdab
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-08-29 15:43:21 +0200

    work on navbar and exmaples

--- SAMPLE_PROJECT/myapp.py
+++ SAMPLE_PROJECT/myapp.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 from flask.ext.jqueryuibootstrap import Bootstrap
-from flask.ext.lwadmin import LwAdmin
+from flask.ext.lwadmin import LwAdmin, Navbar
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -11,10 +11,27 @@ Bootstrap(app)
 # LwAdmin
 LwAdmin(app)
 
+navbar = Navbar()
+navbar.set_brand('Test Project')
+navbar.set_username('anonymous')
+navbar.add_item('app.homepage', 'Homepage')
+navbar.add_item('app.about', 'About')
+navbar.add_item('app.contact', 'Contact')
+
+print navbar.get_data()
 
 @app.route('/')
-def index():
-    return render_template('example.html')
+def homepage():
+    print navbar.get_menu()
+    return render_template('homepage.html', lw_navbar=navbar)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', lw_navbar=navbar)
 
 
 if __name__ == '__main__':
--- flask_lwadmin/__init__.py
+++ flask_lwadmin/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf8
 
-from flask import Blueprint
+from flask import Blueprint, url_for
 
 
 class LwAdmin(object):
@@ -27,43 +27,73 @@ class Navbar:
     URL_EXTERNAL = 2
     DIVIDER = 3
 
-    _navbar = {'brand': 'Project Name', 'username': None, 'items': [], 'profile_items': []}
+    _navbar = {'brand': 'Project Name', 'user': None, 'items': [], 'profile_items': []}
     _items = {}
 
     def set_brand(self, brand):
         self._navbar['brand'] = brand
 
-    def set_username(self, username):
-        self._navbar['username'] = username
+    def set_username(self, username, url=None, type=None, controller=None, disabled=False):
+        item = self.__create_base_item(username, url, type, controller, disabled)
+        self._navbar['user'] = item
 
     def add_item(self, key, label, url=None, type=None, controller=None, disabled=False):
+        self.__check_key(key)
         item = self.__create_base_item(label, url, type, controller, disabled)
         item['menus'] = []
         self._items[key] = item
         self._navbar['items'].append(key)
 
     def add_sub_item(self, parent_key, key, label, url=None, type=None, controller=None, disabled=False):
+        self.__check_key(key)
         item = self.__create_base_item(label, url, type, controller, disabled)
+        item['parent'] = parent_key
         self._items[key] = item
         self._items[parent_key]['menus'].append(key)
 
     def add_profile_item(self, key, label, url=None, type=None, controller=None, disabled=False):
+        self.__check_key(key)
         item = self.__create_base_item(label, url, type, controller, disabled)
         self._items[key] = item
         self._navbar['profile_items'].append(key)
 
     def get_item(self, key):
+        if key not in self._items:
+            RuntimeError('This key: %s not exists' % key)
         return self._items[key]
 
-    def get_navbar(self):
+    def get_data(self):
         return self._navbar
+
+    def get_brand(self):
+        return self._navbar['brand']
+
+    def get_user(self):
+        return self._navbar['user']
+
+    def get_menu(self):
+        menu = []
+        for key in self._navbar['items']:
+            item = self.get_item(key)
+            if item['type'] == self.URL_INTERNAL:
+                item['url'] = url_for(item['url'])
+            menu.append(item)
+        return menu
 
     def __create_base_item(self, label, url=None, type=None, controller=None, disabled=False):
         if type is None:
             type = self.NO_URL
 
-        item = {'label': label, 'type': type, 'url': url, 'disabled': disabled}
+        if type == self.NO_URL:
+            url = '#'
+
+        item = {'label': label, 'type': type, 'url': url, 'disabled': disabled, 'active': False}
         if controller is not None:
             item['controller'] = controller
 
         return item
+
+    def __check_key(self, key):
+        if key in self._items:
+            RuntimeError('This key: %s is not unique' % key)
+        return True
