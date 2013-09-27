9913b061bfd7c98c5f7c00db087e77b0648811e6
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-09-27 08:22:55 +0200

    working on list

--- flask_lwadmin/config.py
+++ flask_lwadmin/config.py
@@ -6,23 +6,32 @@ __author__ = 'ldath'
 class ConfigParser:
 
     def __init__(self):
-        self.list_configuration = {'batch_actions': None, 'object_actions': None}
+        self.list_configuration = dict(actions=None, batch_actions=None, object_actions=None)
 
     def configure(self):
+        if 'actions' in self.list_configuration.keys():
+            self.parse_list_actions()
+
         if 'batch_actions' in self.list_configuration.keys():
-            self.parse_batch_actions()
+            self.parse_list_batch_actions()
 
         if 'object_actions' in self.list_configuration.keys():
-            self.parse_object_actions()
+            self.parse_list_object_actions()
 
-    def parse_batch_actions(self):
+    def parse_list_actions(self):
         pass
 
-    def parse_object_actions(self):
+    def parse_list_batch_actions(self):
         pass
 
-    def get_batch_actions(self):
+    def parse_list_object_actions(self):
+        pass
+
+    def get_list_actions(self):
         return self.list_configuration.get('batch_actions', None)
 
-    def get_object_actions(self):
+    def get_list_batch_actions(self):
+        return self.list_configuration.get('batch_actions', None)
+
+    def get_list_object_actions(self):
         return self.list_configuration.get('object_actions', None)
