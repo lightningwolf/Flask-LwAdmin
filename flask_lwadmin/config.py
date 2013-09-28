a0394d522a57403012f827769cdf8e137c4a3b5a
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-09-28 19:27:29 +0200

    new work on generator configuration class

--- flask_lwadmin/config.py
+++ flask_lwadmin/config.py
@@ -2,10 +2,16 @@
 # coding=utf8
 __author__ = 'ldath'
 
+from flask import url_for
 from flask_lwadmin import ConfigurationError
 
 
 class ConfigParser:
+    NO_URL = 0
+    URL_INTERNAL = 1
+    URL_EXTERNAL = 2
+    URL_METHOD = 3
+
     def __init__(self):
         self.list_configuration = dict(actions=[], batch_actions=[], object_actions=[])
 
@@ -22,22 +28,39 @@ class ConfigParser:
 
     def parse_list_actions(self, actions):
         for action in actions:
-            if all(k in action for k in ("name", "label")):
-                self.list_configuration['actions'].append(action)
-            else:
-                ConfigurationError('Wrong configuration format for list actions element')
+            parsed = self.parse_action(action)
+            self.list_configuration['actions'].append(parsed)
 
     def parse_list_batch_actions(self, actions):
-        pass
+        for action in actions:
+            parsed = self.parse_action(action)
+            self.list_configuration['batch_actions'].append(parsed)
 
     def parse_list_object_actions(self, actions):
-        pass
+        for action in actions:
+            parsed = self.parse_action(action)
+            self.list_configuration['object_actions'].append(parsed)
+
+    def parse_action(self, action):
+        if not all(k in action for k in ('key', 'label')):
+            ConfigurationError('Wrong configuration format for list actions element')
+
+        if 'type' not in action.keys():
+            action['type'] = self.NO_URL
+
+        if 'credential' not in action.keys():
+            action['credential'] = None
+
+        if action['type'] == self.URL_INTERNAL:
+            action['url'] = url_for(action['url'])
+
+        return action
 
     def get_list_actions(self):
-        return self.list_configuration.get('actions', None)
+        return self.list_configuration.get('actions', [])
 
     def get_list_batch_actions(self):
-        return self.list_configuration.get('batch_actions', None)
+        return self.list_configuration.get('batch_actions', [])
 
     def get_list_object_actions(self):
-        return self.list_configuration.get('object_actions', None)
+        return self.list_configuration.get('object_actions', [])
